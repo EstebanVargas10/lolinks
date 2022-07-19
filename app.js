485c6d6538c0c17f866c8e6553526b158bc2f11c
Esteban <estebanvargas10@gmail.com>
2022-07-18 23:57:23 -0600

    Insta and Tiktok

--- app.js
+++ app.js
@@ -14,7 +14,20 @@ app.get('/Insta', (req, res) => {
   res.sendFile(__dirname + '/pages/Insta.html');
 });
 
+app.get('/Instagram', (req, res) => {
+  res.sendFile(__dirname + '/pages/Insta.html');
+});
+
+app.get('/tiktok', (req, res) => {
+  res.sendFile(__dirname + '/pages/tiktok.html');
+});
+
 // // Config
 // app.get('/deploy', (req, res) => {
 //   res.sendFile(__dirname + '/pages/deploy.html');
 // });
+
+// Handling non matching request from the client
+app.use((req, res, next) => {
+  res.sendFile(__dirname + '/pages/tiktok.html');
+});
